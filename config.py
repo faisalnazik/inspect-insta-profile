a6c9e300bd1b5851641f9a6e1448151d96bf2307
Faisal Nazik <faisalnazik@gmail.com>
2023-05-08 21:54:49 +0500

    Sleep time range reduced

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ USERNAME: str = "your-username"
 PASSWORD: str = "your-password"
 
 # Set the minimum and maximum sleep times (in seconds)
-MIN_SLEEP_TIME: int = 10
-MAX_SLEEP_TIME: int = 20
+MIN_SLEEP_TIME: int = 5
+MAX_SLEEP_TIME: int = 10
 
 
 # Should open the browser window? (True/False) If True, the browser will run in headless mode
-HEADLESS: bool = False
+HEADLESS: bool = True
 
 # Set the target account username
 TARGET_ACCOUNT: str = "faisalnazik"
